80452a22f145ee734a0cd40ca32d755fd6b64571
Daniel Assumpção <dannboyy@gmail.com>
2014-05-10 22:06:55 -0300

    Versão 5.1
    Alteração do nome da página e retirada de arquivos desnecessários.

--- Reclamonibus/reclama.php
+++ Reclamonibus/reclama.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Reclamônibus</title>
+<title>Reclamônibus - Reclame Aqui</title>
 
 <link href="style.css" rel="stylesheet" type="text/css" />
 <link href="view.css" rel="stylesheet" type="text/css" />
